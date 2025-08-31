53a0ae36a7a7e212d68c9d8648d4892c78edcb09
phoopyaesone <phoopyaesone984@gmail.com>
2025-08-31 13:48:05 +0630

    Fixed DisallowedHost error by adding Render domain

--- uniHub/settings.py
+++ uniHub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j%v^1^+-hq)_0#fdakx&668t)9usq^fw!eh6@z7ec#9xekq3r#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'uniinfohub-1.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'uniinfohub.onrender.com']
 
 # Application definition
 
